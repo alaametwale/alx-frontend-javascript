7abc5698c21037060a47aefe2ae89e7cfbf159f7
alaametwale <alaameto86@gmail.com>
2025-11-08 14:36:52 +0200

    fix: correct printTeacher and constructor

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -1,3 +1,4 @@
+// Define Teacher interface
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
@@ -7,15 +8,61 @@ interface Teacher {
   [key: string]: any;
 }
 
+// Define Directors interface extending Teacher
 interface Directors extends Teacher {
   numberOfReports: number;
 }
 
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
+// 3. Printing teachers
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
-  const firstInitial: string = firstName.charAt(0);
-  return firstInitial + '. ' + lastName;
-};
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+console.log(printTeacher("John", "Doe"));
+
+// 4. Writing a class
+interface StudentClassConstructor {
+  firstName: string;
+  lastName: string;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor({ firstName, lastName }: StudentClassConstructor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student = new StudentClass({ firstName: "Mary", lastName: "Smith" });
+console.log(student.displayName());
+console.log(student.workOnHomework());
